0bc919634ca0ba0bf37b4003ea02c742cd4e98f7
l33tllama <overclockedamiga@gmail.com>
2016-02-05 19:41:29 +1100

    Forgot encoder class. Adding it.

---
+++ encoder.cpp
@@ -0,0 +1,49 @@
+#include "encoder.h"
+#include <Arduino.h>
+
+Encoder::Encoder(){
+  
+}
+
+Encoder::Encoder(unsigned int enc_a_pin, unsigned int enc_b_pin, unsigned int enc_btn_pin){
+  // setup rotary encoder
+  pinMode(enc_a_pin, INPUT);
+  pinMode(enc_b_pin, INPUT);
+  pinMode(enc_btn_pin, INPUT);
+
+  _enc_a_pin = enc_a_pin;
+  _enc_b_pin = enc_b_pin;
+  _enc_btn_pin = enc_btn_pin;
+
+  encoderPos = 0;
+  lastEncoderPos = 0;
+  encoderPinALast = LOW;
+  n = LOW;
+}
+
+encState Encoder::read(){
+  lastEncoderPos = encoderPos;
+  //Serial.println("Reading encoder..");
+  n = digitalRead(_enc_a_pin);
+  if ((encoderPinALast == LOW) && (n == HIGH)) {
+    if (digitalRead(_enc_b_pin) == LOW) {
+       encoderPos--;
+     } else {
+       encoderPos++;
+     }
+     Serial.print (encoderPos);
+     Serial.println ("/");
+   } 
+   encoderPinALast = n;
+    if(encoderPos == lastEncoderPos){
+      return ENC_SAME;
+    } else if (encoderPos > lastEncoderPos){
+      Serial.println("Inc");
+      
+      return ENC_DEC;
+    } else if (encoderPos < lastEncoderPos){
+      Serial.println("Dec");
+      return ENC_INC;
+    }
+   return ENC_SAME;
+}
